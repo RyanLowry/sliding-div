3a66f973aa54824606fb015ee3ce6a4e14f31517
RyanLowry <ryanlowry9@gmail.com>
2019-07-24 09:52:19 -0400

    Add test files

---
+++ test/scripts/main.js
@@ -0,0 +1,6 @@
+(function(){
+	let sliding = new SlidingDiv(document.getElementById('image-holder'));
+    sliding.apply()
+    let sliding2 = new SlidingDiv(document.getElementById('image-holder2'));
+	sliding2.apply()
+})();
